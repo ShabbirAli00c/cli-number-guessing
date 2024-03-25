dd5e2047dab27fa60c3c17d1bf2533c77eea555f
ShabbirAli00c <159994827+ShabbirAli00c@users.noreply.github.com>
2024-03-25 12:10:26 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,17 @@
+console.log("Guess a number b/w 1 to 10");
+//now variable create
+let a:number=Math.floor(Math.random()*10)
+import inquirer from "inquirer";
+while(true){
+    let input = await inquirer.prompt
+        ({name: "guess", type: "number",
+        message:"Enter your guess number you want: "})
+
+    //PROVIDE CODE
+    let ans:answer = input.guess
+    if (a == ans)
+    {console.log("congratulation your guess number is correct")
+    break;}
+    else{console.log("Sorry you guess wrong number try again ")}
+}
+
